86da233f904a8347fa8837c22e7050d7f4bbe0f3
darshanaBora <73940952+darshanaBora@users.noreply.github.com>
2023-04-09 13:22:42 +0530

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,36 @@
+import cv2
+
+
+# Create our body classifier
+body_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Initiate video capture for video file
+cap = cv2.VideoCapture('walking.avi')
+
+# Loop once video is successfully loaded
+while True:
+    
+    # Read first frame
+    ret, frame = cap.read()
+
+    #Convert Each Frame into Grayscale
+    gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Pass frame to our body classifier
+    bodies = body_classifier.detectMultiScale(gray,1.1, 5)
+    
+    # Extract bounding boxes for any bodies identified
+    for (x,y,w,h) in bodies:
+       
+       cv2.rectangle(bodies,(x,y),(x+w,y+h),(255,0,0),2)  
+
+       # Crop the image to save the face image.
+       roi_color = bodies[y:y+h, x:x+w]
+       cv2.imwrite("body.jpg",roi_color)
+
+    if cv2.waitKey(1) == 32: #32 is the Space Key
+        break
+
+cv2.imshow('bodies', bodies)
+cap.release()
+cv2.destroyAllWindows()
